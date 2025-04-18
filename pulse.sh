9d2179cd4cf52fa904a80e646a4e609028629414
Ether-G <toastedben@gmail.com>
2025-04-17 18:11:17 -0600

    adding license and cleanup

--- pulse.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Export the display for X11 applications
-export DISPLAY=:0
-# Set PULSE_SERVER to use the Windows host's PulseAudio server
-export PULSE_SERVER=tcp:$(grep nameserver /etc/resolv.conf | awk '{print $2}');
-echo "PulseAudio server set to $PULSE_SERVER"
